997bcd329bc8905e08b3b6a6bd1ce24e64c2176c
Craig Oliver <craig_oliver@wycliffeassociates.org>
2021-08-25 14:01:52 -0400

    Footnote fixes (#9)
    * WIP Added test app to review formatting
    
    * Make footnote caller and targets link to each other
    
    * Remove test app
    
    * Revert solution changes
    
    * WIP Fixing tests
    
    * Fixed remaining tests
    
    * Reformatted tests
    
    * Whitespace
    
    * Reset footnote ids on render
    
    * Add hr after footnotes to visually break with next chapter
    
    * Style hr to be more subtle

--- USFMToolsSharp.Renderers.HTML/HtmlRenderer.cs
+++ USFMToolsSharp.Renderers.HTML/HtmlRenderer.cs
@@ -18,7 +18,8 @@ namespace USFMToolsSharp.Renderers.HTML
         private IList<string> TOCEntries;
         private CMarker CurrentChapter;
         private VMarker CurrentVerse;
-        private USFMDocument document;
+        private int NextFootnoteUniqueID = 1;
+	private USFMDocument document;
 
         public string FrontMatterHTML { get; set; }
         public string InsertedFooter { get; set;}
@@ -42,6 +43,7 @@ namespace USFMToolsSharp.Renderers.HTML
             UnrenderableTags = new List<string>();
             var encoding = GetEncoding(input);
             StringBuilder output = new StringBuilder();
+            NextFootnoteUniqueID = 1;
 
             if (!ConfigurationHTML.partialHTML)
             {
@@ -347,9 +349,11 @@ namespace USFMToolsSharp.Renderers.HTML
                             footnoteId = fMarker.FootNoteCaller;
                             break;
                     }
-                    string footnoteCallerHTML = $"<sup class=\"caller\">{footnoteId}</sup>";
+                    string footnoteCallerHTML = $"<sup id=\"footnote-caller-{NextFootnoteUniqueID}\" class=\"caller\"><a href=\"#footnote-target-{NextFootnoteUniqueID}\">{footnoteId}</a></sup>";
+                    string footnoteTargetHTML = $"<sup id=\"footnote-target-{NextFootnoteUniqueID}\" class=\"caller\"><a href=\"#footnote-caller-{NextFootnoteUniqueID}\">{footnoteId}</a></sup>";
+                    NextFootnoteUniqueID++;
                     output.AppendLine(footnoteCallerHTML);
-                    footnote.Append(footnoteCallerHTML);
+                    footnote.Append(footnoteTargetHTML);
                     foreach (Marker marker in input.Contents)
                     {
                         footnote.Append(RenderMarker(marker));
@@ -781,6 +785,7 @@ namespace USFMToolsSharp.Renderers.HTML
                     footnoteHTML.Append(footnote);
                     footnoteHTML.AppendLine("</div>");
                 }
+                footnoteHTML.AppendLine("<hr/>");
                 FootnoteTextTags.Clear();
                 return footnoteHTML.ToString();
             }
